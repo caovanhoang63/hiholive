6bb6ccd239e388fb0ca928f98f8d26791f38c303
hoangcao <22520457@gm.uit.edu.vn>
2024-12-15 02:43:29 +0700

    inc auto close time

--- services/hls_mux/module/transport/hlsgrpc/grpc.go
+++ services/hls_mux/module/transport/hlsgrpc/grpc.go
@@ -27,7 +27,7 @@ func (h *hlsGRPC) NewHlsStream(ctx context.Context, req *pb.NewHlsStreamReq) (*p
 		fn := h.Ffmpeg.NewStream(streamId, link, key, int(fps), int(resolution))
 
 		go func() {
-			time.Sleep(20 * time.Second)
+			time.Sleep(50 * time.Second)
 
 			for i := 0; i < 10; i++ {
 				fmt.Println(i)
