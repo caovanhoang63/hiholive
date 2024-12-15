107cbb93f7e41220c4ddcca5a226f86f34115fbe
hoangcao <22520457@gm.uit.edu.vn>
2024-12-15 19:41:45 +0700

    remove s3fs

--- services/hls_mux/cmd/root.go
+++ services/hls_mux/cmd/root.go
@@ -5,12 +5,9 @@ import (
 	"github.com/caovanhoang63/hiholive/services/hls_mux/component/ffmpegc"
 	"github.com/caovanhoang63/hiholive/shared/go/core"
 	"github.com/caovanhoang63/hiholive/shared/go/srvctx"
-	"github.com/caovanhoang63/hiholive/shared/go/srvctx/components/ginc"
-	"github.com/caovanhoang63/hiholive/shared/go/srvctx/components/ginc/middlewares"
 	"github.com/caovanhoang63/hiholive/shared/go/srvctx/components/gormc"
 	"github.com/caovanhoang63/hiholive/shared/go/srvctx/components/jwtc"
 	"github.com/caovanhoang63/hiholive/shared/go/srvctx/components/redisc"
-	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 
 	"os"
@@ -19,12 +16,10 @@ import (
 func newServiceCtx() srvctx.ServiceContext {
 	return srvctx.NewServiceContext(
 		srvctx.WithName("Demo Microservices"),
-		srvctx.WithComponent(ginc.NewGin(core.KeyCompGIN)),
 		srvctx.WithComponent(gormc.NewGormDB(core.KeyCompMySQL, "")),
 		srvctx.WithComponent(jwtc.NewJWT(core.KeyCompJWT)),
 		srvctx.WithComponent(core.NewConfig()),
 		srvctx.WithComponent(redisc.NewRedis(core.KeyRedis)),
-		//srvctx.WithComponent(rabbitpubsub.NewRabbitPubSub(core.KeyCompRabbitMQ)),
 	)
 }
 
@@ -39,24 +34,11 @@ var rootCmd = &cobra.Command{
 			logger.Fatal(err)
 		}
 
-		ginComp := serviceCtx.MustGet(core.KeyCompGIN).(core.GINComponent)
 		rd := serviceCtx.MustGet(core.KeyRedis).(core.RedisComponent)
-		router := ginComp.GetRouter()
-
-		router.Static("/static", "hls_output")
-		router.Use(middlewares.Cors())
-
-		router.GET("ping", func(c *gin.Context) {
-			c.JSON(200, "pong")
-		})
 
 		ffmpeg := ffmpegc.NewFfmpeg(serviceCtx).WithConfig(ffmpegc.NewFfmpegConfig("./hls_output", rd.GetClient()))
 
 		go StartGRPCServices(ffmpeg, serviceCtx)
-
-		if err := router.Run(fmt.Sprintf(":%d", ginComp.GetPort())); err != nil {
-			logger.Fatal(err)
-		}
 	},
 }
 
