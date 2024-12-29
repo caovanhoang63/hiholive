641f8ecd64759a8dd600cd2566cdb42a5022899c
hoangcao <22520457@gm.uit.edu.vn>
2024-12-29 15:01:53 +0700

    update hls

--- services/hls_mux/module/transport/hlsgrpc/grpc.go
+++ services/hls_mux/module/transport/hlsgrpc/grpc.go
@@ -1,11 +1,9 @@
 package hlsgrpc
 
 import (
-	"fmt"
 	"github.com/caovanhoang63/hiholive/services/hls_mux/component/ffmpegc"
 	"github.com/caovanhoang63/hiholive/shared/go/proto/pb"
 	"golang.org/x/net/context"
-	"time"
 )
 
 type hlsGRPC struct {
@@ -24,16 +22,16 @@ func (h *hlsGRPC) NewHlsStream(ctx context.Context, req *pb.NewHlsStreamReq) (*p
 	streamId := req.StreamId
 
 	go func() {
-		fn := h.Ffmpeg.NewStream(streamId, link, key, int(fps), int(resolution))
+		_ = h.Ffmpeg.NewStream(streamId, link, key, int(fps), int(resolution))
 
-		go func() {
-			time.Sleep(50 * time.Second)
-
-			for i := 0; i < 10; i++ {
-				fmt.Println(i)
-			}
-			fn()
-		}()
+		//go func() {
+		//	time.Sleep(50 * time.Second)
+		//
+		//	for i := 0; i < 10; i++ {
+		//		fmt.Println(i)
+		//	}
+		//	fn()
+		//}()
 
 	}()
 
