2c5ea7d239a0c84b62cd035012361226a05a8655
hoangcao <22520457@gm.uit.edu.vn>
2024-12-31 14:15:41 +0700

    feat: add api check pin

--- services/auth/module/auth/transport/authapi/ginapi.go
+++ services/auth/module/auth/transport/authapi/ginapi.go
@@ -40,6 +40,20 @@ func (g *ginAPI) ResetPassword() func(c *gin.Context) {
 
 func (g *ginAPI) CheckForgotPasswordPin() func(c *gin.Context) {
 	return func(c *gin.Context) {
+		type Pin struct {
+			Pin   string `json:"pin"`
+			Email string `json:"email"`
+		}
+		var pin Pin
+		if err := c.ShouldBind(&pin); err != nil {
+			core.WriteErrorResponse(c, core.ErrBadRequest)
+			return
+		}
+		if err := g.biz.CheckForgotPasswordPin(c.Request.Context(), pin.Email, pin.Pin); err != nil {
+			core.WriteErrorResponse(c, err)
+			return
+		}
+		c.JSON(http.StatusOK, core.ResponseData(true))
 
 	}
 }
